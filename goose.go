558f1265d30d2af71b4ffb566503b9ada794462f
Victor Häggqvist <victor@snilius.com>
2016-02-04 00:25:06 +0100

    pflag and don't export be default

--- goose.go
+++ goose.go
@@ -1,36 +1,52 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
 	"github.com/gorilla/handlers"
+	flag "snilius.com/pflag"
 )
 
+const version = "1.0"
+const usage = `
+Usage: goose [options...] [webroot]
+webroot defaults to current working directory
+
+Options:
+`
+
 var (
-	port    int
-	webroot string
+	port   int
+	export bool
 )
 
 func main() {
-	flag.IntVar(&port, "p", 8080, "Port to bind")
-	flag.StringVar(&webroot, "r", "", "Webroot (default cwd)")
+	flag.IntVarP(&port, "port", "p", 8080, "Port to bind")
+	flag.BoolVarP(&export, "export", "e", false, "Bind server to 0.0.0.0")
+	flag.Version = version
+	flag.Usage = func() {
+		flag.PrintVersion()
+		fmt.Println(usage)
+		flag.PrintDefaults()
+	}
 	flag.Parse()
 
-	if webroot == "" {
-		if len(flag.Args()) > 0 {
-			webroot = flag.Args()[0]
-		} else {
-			cwd, _ := os.Getwd()
-			webroot = cwd
-		}
+	var webroot string
+	if len(flag.Args()) > 0 {
+		webroot = flag.Args()[0]
+	} else {
+		cwd, _ := os.Getwd()
+		webroot = cwd
 	}
 
 	fmt.Printf("Serving from %s\n", webroot)
-	host := fmt.Sprintf(":%d", port)
+	host := fmt.Sprintf("127.0.0.1:%d", port)
+	if export {
+		host = fmt.Sprintf("0.0.0.0:%d", port)
+	}
 	fmt.Printf("Binding to %s\n", host)
 
 	fileServer := http.FileServer(http.Dir(webroot))
